42a93cef02e5acb1a4aae269baed907394658afe
drinkingstraw33 <60614681+drinkingstraw33@users.noreply.github.com>
2020-02-24 19:51:58 +0900

    grid!!!

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+$(document).ready(function () {
+    $("#menutoggle").click(function () {
+        if ($("#wrap").hasClass("off") === false) {
+            $("#wrap").addClass("off");
+            $("header").addClass("off");
+            $("footer").addClass("off");
+        } else {
+            $("#wrap").removeClass("off");
+            $("header").removeClass("off");
+            $("footer").removeClass("off");
+        }
+    });
+});
+
+$(document).ready(function () {
+    $("#nighttoggle").click(
+        function nightDayHandler() {
+            if ($("#nighttoggle").hasClass("on") === false) {
+                $("#nighttoggle").addClass("on");
+                Outer.setTitleColor('#4E2700');
+                Outer.setHeaderColor('#272100');
+                Body.setBackgroundColor('#181800');
+                Body.setColor('white');
+                Links.setColor('#74BAEC');
+                Footer.setInstarColor('#008453');
+                Footer.setFaceColor('#522719');
+                Footer.setColor('#626671');
+            } else {
+                $("#nighttoggle").removeClass("on");
+                Outer.setTitleColor('gold');
+                Outer.setHeaderColor('palegoldenrod');
+                Body.setBackgroundColor('lightgoldenrodyellow');
+                Body.setColor('black');
+                Links.setColor('saddlebrown');
+                Footer.setInstarColor('pink');
+                Footer.setFaceColor('lightblue');
+                Footer.setColor('gray');
+            }
+        }
+    );
+});
+
+
+
 var Outer = {
     setTitleColor: function (color) {
         $('#title').css('backgroundColor', color);
@@ -5,7 +49,7 @@ var Outer = {
     setHeaderColor: function (color) {
         $('header').css('backgroundColor', color);
     }
-}
+};
 
 var Body = {
     setColor: function (color) {
@@ -14,13 +58,13 @@ var Body = {
     setBackgroundColor: function (color) {
         $('body').css('backgroundColor', color);
     }
-}
+};
 
 var Links = {
     setColor: function (color) {
         $('a').css('color', color);
     }
-}
+};
 
 var Footer = {
     setInstarColor: function (color) {
@@ -30,36 +74,9 @@ var Footer = {
         $('.face').css('backgroundColor', color);
     },
     setColor: function (color) {
-    $('footer').css('color', color);
+        $('footer').css('color', color);
     },
     setBackgroundColor: function (color) {
-    $('footer').css('backgroundColor', color)
-}
-}
-
-function nightDayHandler(self) {
-    var target = document.querySelector('body');
-    if (self.value === '야간 모드로 전환') {
-        self.value = '야간 모드 해제';
-        Outer.setTitleColor('#4E2700');
-        Outer.setHeaderColor('#272100');
-        Body.setBackgroundColor('#181800');
-        Body.setColor('white');
-        Links.setColor('#74BAEC');
-        Footer.setInstarColor('#008453');
-        Footer.setFaceColor('#522719');
-        Footer.setColor('#626671');
-        Footer.setBackgroundColor('#272100');
-    } else {
-        self.value = '야간 모드로 전환';
-        Outer.setTitleColor('gold');
-        Outer.setHeaderColor('palegoldenrod');
-        Body.setBackgroundColor('lightgoldenrodyellow');
-        Body.setColor('black');
-        Links.setColor('saddlebrown');
-        Footer.setInstarColor('pink');
-        Footer.setFaceColor('lightblue');
-        Footer.setColor('gray');
-        Footer.setBackgroundColor('palegoldenrod');
+        $('footer').css('backgroundColor', color)
     }
-}
+};
